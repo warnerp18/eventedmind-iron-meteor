b8ed71096a406b45d781b9c86a3777a1ec899d05
Phil <warnerp18@gmail.com>
2015-07-19 13:37:27 -0500

    creating app with eventedmind iron-meteor class

---
+++ config/development/env.sh
@@ -0,0 +1,12 @@
+# This .sh file will be sourced before starting your application.
+# You can use it to put environment variables you want accessible
+# to the server side of your app by using process.env.MY_VAR
+#
+# Example:
+# export MONGO_URL="mongodb://localhost:27017/myapp-development"
+# export ROOT_URL="http://localhost:3000"
+
+export SAMPLE_VARIABLE="somevalue"
+
+export ACCOUNTS_METEOR_ID='cWCFW2GRwXiGsfjdr'
+export ACCOUNTS_METEOR_SECRET='NccqrRqmu2tALZhgRXDhhr9JB77wuhoTcq'
